48a8498c4377a4936b1ba009c5f8b737a316eafb
Marc Koderer <marc@koderer.com>
2022-12-12 13:58:16 +0100

    Add vagrant dev env

---
+++ vagrant/install.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install -y libncurses-dev gawk flex bison bc
+sudo apt-get install -y openssl libssl-dev dkms libelf-dev
+sudo apt-get install -y libudev-dev libpci-dev libiberty-dev autoconf libssl-dev
+sudo apt-get install -y git makea
+# Optional
+sudo apt install linux-headers-$(uname -r)
+
+#git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+
